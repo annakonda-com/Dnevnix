d266ef4a8f4dfb57f217116f550885585bb03d35
unknown <annakondr0709a@gmail.com>
2025-09-28 15:07:31 +0300

    idk

--- app/src/main/java/ru/annakondr/dnevnix/ui/screens/CoachScreen.kt
+++ app/src/main/java/ru/annakondr/dnevnix/ui/screens/CoachScreen.kt
@@ -1,11 +1,31 @@
 package ru.annakondr.dnevnix.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.annakondr.dnevnix.R
 import ru.annakondr.dnevnix.navigation.NavigationManager
 
+@Preview
 @Composable
-fun CoachScreenUi(modifier : Modifier, navigationManager: NavigationManager){
-    Text(modifier = modifier, text = "Репетитор")
+fun CoachScreenUi(){
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {},
+            placeholder = { Text(text = stringResource(R.string.help)) }
+        )
+    }
 }
